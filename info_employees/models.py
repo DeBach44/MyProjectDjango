544384170d7c7805c91ac13080ed51d3bcf31b8a
DeBach44 <dbacherikov97@mail.ru>
2025-09-13 10:17:26 +0300

    Make Models Employee and Workplace, make gitignore

--- info_employees/models.py
+++ info_employees/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
+class Employee(models.Model):
+    '''Класс сотрудники'''
+    GENDER_CHOICES = [
+        ('М', 'Мужской'),
+        ('Ж', 'Женский'),
+    ]
+    first_name = models.CharField(max_length=30, verbose_name='Имя')
+    last_name = models.CharField(max_length=30, verbose_name='Фамилия')
+    patronymic = models.CharField(max_length=30, verbose_name='Отчество', blank=True, null=True)
+    skills = models.TextField(verbose_name='Навыки')
+    level = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(10)],
+        verbose_name='Уровень навыков'
+    )
+    gender = models.CharField(
+        max_length=1,
+        choices=GENDER_CHOICES,
+        verbose_name='Пол'
+    )
+    class Meta:
+        verbose_name = 'Сотрудник'
+        verbose_name_plural = 'Сотрудники'
+
+    def __str__(self):
+        return f"{self.last_name} {self.first_name} {self.middle_name}"
--- info_workplaces/models.py
+++ info_workplaces/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from info_employees.models import Employee
 
 # Create your models here.
+class Workplace(models.Model):
+    '''Рабочие места'''
+    number = models.CharField(
+        max_length=20,
+        unique=True,
+        verbose_name='Номер рабочего места'
+        )
+    employee = models.OneToOneField(Employee, 
+        on_delete=models.SET_NULL, null=True,
+        verbose_name='Сотрудник'
+        )
+    
+    class Meta:
+        verbose_name = 'Рабочее место'
+        verbose_name_plural = 'Рабочие места'
+    
+    def __str__(self):
+        return self.number
